0f59cfb9aa476931787a1ffda60f10ecf4a8f0c7
vikkramstark <vikkram26stark@gmail.com>
2024-01-10 02:37:56 +0530

    Created FrontEnd

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express"); 
 const app = express()
+const expressLayout = require("express-ejs-layouts");  
 
 
 const mongoose = require("mongoose"); 
@@ -18,14 +19,27 @@ db.once("open", () => {
 
 const Blog = require("./model"); 
 
-
+app.use(expressLayout) 
+app.use(express.urlencoded({extended:false}))  
+app.set("view engine", "ejs")  
+app.set("views", "./views") 
+app.set("layout", "layouts/layout")   
 app.use(express.json()) 
 
 // ROUTES DEFINED HERE 
 
-app.get("/", (req,res) => {
-    res.send("Welcome TO Blog App!");  
+app.get("/", async(req,res) => {
+    try{
+        const blogs = await Blog.find();
+        res.render("index.ejs", {blogs:blogs});     
+    }catch(err){
+        console.log(err); 
+    }
 })
+
+app.get("/add", (req,res) => {
+    res.render("addBlog.ejs");  
+});   
 
 app.get("/getblogs", async (req,res) => {
     try{
@@ -41,7 +55,8 @@ app.post("/postblog", async (req,res) => {
     try{
         const blog = new Blog({title:req.body.title, content:req.body.content, author:req.body.author});  
         const newBlog = await blog.save(); 
-        res.status(201).send("Blog Added Successfully!") 
+        // res.status(201).send("Blog Added Successfully!") 
+        res.redirect("/")  
     }catch(err){
         console.log(err); 
         res.status(500).send("Unexpected Error Occured!")  
@@ -53,3 +68,4 @@ const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`[SERVER STARTED] : http://localhost:${PORT}`)   
 })  
+
